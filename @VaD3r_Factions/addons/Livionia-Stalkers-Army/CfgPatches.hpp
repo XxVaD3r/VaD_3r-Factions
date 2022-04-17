118c44df65d6a57f29ebee93395f99794955695e
XxVaD3r <dima.simmons435@gmail.com>
2022-04-17 14:04:50 -0600

    Fixed Livionia Army not showing up in zeus Fixed mismatched unit names causing Livionia Army to not show up in zeus. Fix found here: http://alivemod.com/forum/2406-orbat-creator-general-faq/20

--- @VaD3r_Factions/addons/Livionia-Stalkers-Army/CfgPatches.hpp
+++ @VaD3r_Factions/addons/Livionia-Stalkers-Army/CfgPatches.hpp
@@ -1,17 +1,17 @@
 class CfgPatches {
     class I_LVStalker {
         units[] = {
-            "I_ILVStalker_Unarmed_01",
-            "I_ILVStalker_Team_Leader_01",
-            "I_ILVStalker_Squad_Leader_01",
-            "I_ILVStalker_Rifleman_01",
-            "I_ILVStalker_Rifleman_AT_01",
-            "I_ILVStalker_Grenadier_01",
-            "I_ILVStalker_Specialist_01",
-            "I_ILVStalker_Machine_Gunner_01",
-            "I_ILVStalker_Machine_Gunner_Assistant_01",
-            "I_ILVStalker_Auto_Rifleman_01",
-            "I_ILVStalker_Sniper_01"
+            "B_Livionian_Stalkers_ArmyArmy_Unarmed_01",
+            "B_Livionian_Stalkers_ArmyArmy_Team_Leader_01",
+            "B_Livionian_Stalkers_ArmyArmy_Squad_Leader_01",
+            "B_Livionian_Stalkers_ArmyArmy_Rifleman_01",
+            "B_Livionian_Stalkers_ArmyArmy_Rifleman_AT_01",
+            "B_Livionian_Stalkers_ArmyArmy_Grenadier_01",
+            "B_Livionian_Stalkers_ArmyArmy_Specialist_01",
+            "B_Livionian_Stalkers_ArmyArmy_Machine_Gunner_01",
+            "B_Livionian_Stalkers_ArmyArmy_Machine_Gunner_Assistant_01",
+            "B_Livionian_Stalkers_ArmyArmy_Auto_Rifleman_01",
+            "B_Livionian_Stalkers_ArmyArmy_Sniper_01"
         };
         weapons[] = {};
         requiredVersion = 1.62;
